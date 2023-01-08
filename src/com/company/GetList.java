a38ba654b4637af1b7a25266784563f362a5e701
Rajan Maharjan <rajanmaharjan042@gmail.com>
2023-01-08 20:34:06 +0545

    Commit

--- src/com/company/GetList.java
+++ src/com/company/GetList.java
@@ -14,7 +14,7 @@ public class GetList {
     public List<Book> getList() throws Exception{
         ObjectMapper mapper = new ObjectMapper();
 
-        String jsonInput = FileUtils.readFileToString(new File("C:\\Users\\Radhika\\IdeaProjects\\Book Recommendation System\\res\\books.json"), StandardCharsets.UTF_8);
+        String jsonInput = FileUtils.readFileToString(new File("res\\books.json"), StandardCharsets.UTF_8);
         List<Book> book = mapper.readValue(jsonInput, new TypeReference<List<Book>>(){});
 
         return book;
